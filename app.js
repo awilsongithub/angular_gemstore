91f0e95798b69f14b08ebf906114cc51cebaab05
Adam Wilson <adamwilson1234@gmail.com>
2016-07-30 15:32:07 -0500

    added classes if condition met in ng-class. Used expression within directive values calling contrller methods to return boolean or numeric values used to ng-show/hide and style elements.

--- app.js
+++ app.js
@@ -9,12 +9,28 @@
         this.products = gems;
     });
 
+    // panel controller
+    app.controller('PanelController', function(){
+        this.tab = 1;
+        // set the tab
+        this.setTab = function(setTab){
+            this.tab = setTab;
+        };
+        // check if selected
+        this.isSet = function(isSet){
+            return this.tab === isSet;
+        };
+    });
+
+    // gallery controller
+    
+
     // data object
     var gems = [
         {
             name: 'Azurite',
             price: 2.95,
-            description: 'blah, blah, blah',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
             canPurchase: false,
             soldOut: false,
             images: [
@@ -23,7 +39,7 @@
         },{
             name: 'Beach Rock',
             price: 2,
-            description: 'blah, blah, blah',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
             canPurchase: true,
             soldOut: false,
             images: [
@@ -32,7 +48,7 @@
         }, {
             name: 'Diamond',
             price: 99.99,
-            description: 'blah, blah, blah',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
             canPurchase: true,
             soldOut: false,
             images: [
