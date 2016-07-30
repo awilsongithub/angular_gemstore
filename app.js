5dbbfa55685a9f4f528c1dbb1c8259f65ecd3db3
Adam Wilson <adamwilson1234@gmail.com>
2016-07-29 22:35:33 -0500

    added currency filter and ng-src images and ng-repeated data items.

--- app.js
+++ app.js
@@ -1,14 +1,45 @@
 // put everything in a closure
 (function(){
-    // data object
-    var gem = { name: 'Azurite', price: 2.95 };
+
     // module
     var app = angular.module('gemStore', [ ]);
 
     // controller
     app.controller('StoreController', function(){
-        this.product = gem;
+        this.products = gems;
     });
+
+    // data object
+    var gems = [
+        {
+            name: 'Azurite',
+            price: 2.95,
+            description: 'blah, blah, blah',
+            canPurchase: false,
+            soldOut: false,
+            images: [
+                'images/Ruby_logo.png'
+            ]
+        },{
+            name: 'Beach Rock',
+            price: 2,
+            description: 'blah, blah, blah',
+            canPurchase: true,
+            soldOut: false,
+            images: [
+                'images/Ruby_logo.png'
+            ]
+        }, {
+            name: 'Diamond',
+            price: 99.99,
+            description: 'blah, blah, blah',
+            canPurchase: true,
+            soldOut: false,
+            images: [
+                'images/Ruby_logo.png'
+            ]
+        }
+    ];
 
 
 
